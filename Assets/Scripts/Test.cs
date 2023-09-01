7681de53c73b1db317e58bfdc9cc021ed0768683
xchu <xchu@in-road.com>
2023-09-02 03:11:22 +1000

    First commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Test : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            Debug.Log("Test ");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
